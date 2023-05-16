2eeeebb67ba11d5412d6bd385ba18d45ac76d520
Flacko <f@ak68a.co>
2023-05-16 09:01:31 -0400

    Create initial view

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ayo Moreira on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
